a9a15bc99a8494c8103d272a80144e8d60f469af
Jayasri P <jayaporalla921@gmail.com>
2025-07-17 12:59:52 +0530

    Feature: added home page

---
+++ travel_app_frontend/src/pages/Home/Home.js
@@ -0,0 +1,72 @@
+import { Fragment, useEffect, useState } from "react";
+import axios from "axios";
+import InfiniteScroll from  'react-infinite-scroll-component';
+
+import { Navbar, HotelCard, Categories } from "../../components";
+import { useCategory } from "../../context";
+import "./Home.css";
+
+export const Home = () => {
+
+    const [hasMore, setHasMore] = useState(true);
+    const [hotelsToShow, setHotelsToShow] = useState([]);
+    const [testData, setTestData] = useState([]);
+    const [currentIndex, setCurrentIndex] = useState(16);
+    const { hotelCategory } = useCategory();
+
+    const [hotels, setHotels] = useState([]);
+
+    useEffect(() => {
+        ( async () => {
+            try{
+                const { data } = await axios.get(
+                    `https://travel-app-backend-uy83.onrender.com/api/hotels?category=${hotelCategory}`
+                );
+                setTestData(data);
+                setHotels(data ? data.slice(0, 16) : []);
+            } catch(err){
+                console.log(err);
+            }
+        })();
+    }, [hotelCategory]);
+
+    const fetchMoreData = () => {
+        if(hotels.length >= testData.length){
+            setHasMore(false)
+            return;
+        }
+        setTimeout(() => {
+            if (hotels && hotels.length > 0){
+                setHotels(hotels.concat(testData.slice(currentIndex, currentIndex + 16))
+            );
+                setCurrentIndex((prev) => prev + 16);
+            } else {
+                setHotels([]);
+            }
+        }, 1000);
+    };
+
+    return (
+        <Fragment>
+            <Navbar />
+            <Categories />
+                {
+                    hotels && hotels.length > 0 ? (
+                        <InfiniteScroll
+                            dataLength={hotels.length}
+                            next={fetchMoreData}
+                            hasMore={hasMore}
+                            loader={hotels.length > 0 && <h3 className="alert-text">Loading...</h3>}
+                            endMessage={<p className="alert-text">You have seen it all!</p>}
+                        >
+                            <main className="main d-flex align-center wrap gap-larger">
+                                {
+                                    hotels && hotels.map((hotel) => ( <HotelCard key={hotel._id} hotel={hotel}/>))
+                                }
+                            </main>
+                        </InfiniteScroll>
+                    ) : (<></>)
+                }
+        </Fragment>
+    )
+}

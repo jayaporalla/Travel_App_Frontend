d294005a1d47d4a1b7c8fc08e76605d2ad7f9783
Jayasri P <jayaporalla921@gmail.com>
2025-07-19 18:48:24 +0530

    Added single hotel page

--- travel_app_frontend/src/App.js
+++ travel_app_frontend/src/App.js
@@ -1,9 +1,14 @@
-import { Home } from "./pages";
+import { Routes, Route } from 'react-router-dom';
+import { Home, SingleHotel, SearchResults } from "./pages";
 import './App.css';
 
 function App() {
   return (
-    <Home />
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/hotels/:name/:address/:id/reserve' element={<SingleHotel />}/>
+      <Route path='/hotels/:address' element={<SearchResults />}/>
+    </Routes>
   );
 }
 
